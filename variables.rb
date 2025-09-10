657ba9c74a6f2d751be719f9beb5b66d2abfd15d
Thewisking <wissamdiab122@gmail.com>
2025-09-10 14:29:06 +0930

    variables learnt

---
+++ variables.rb
@@ -0,0 +1,23 @@
+system("cls")
+
+x = "35"
+y = 32
+isMale = true
+isTall = false
+flaws = nil
+
+puts ("x = " + x)
+
+x = "     neGAwATt"
+puts ("x = " + x)
+
+# this video is taking forever to let me code stff 3 files with like 30 lines in 35 minutes...
+
+
+puts "strip: " + x.strip
+puts "upcase: " + x.upcase
+puts "length: " + x.length.to_s
+puts "include ? watt: " + x.include?("watt").to_s
+puts "index 9: " + x[9]
+puts "print the 8 letters after index 5 including 5: " + x[5,8]
+puts "Where is A? " + x.index("A").to_s 
