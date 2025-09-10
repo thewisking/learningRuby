846a5d75b4962dca7efaed5f6569f9cc3e9aaf91
Thewisking <wissamdiab122@gmail.com>
2025-09-10 18:37:25 +0930

    for loops done

---
+++ forLoops.rb
@@ -0,0 +1,18 @@
+system("cls")
+
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+
+# for friend in friends
+#   puts friend
+# end
+
+# friends.each do |s|
+#   puts s
+# end
+
+
+for index in 0..5
+  puts friends[index]
+end
+
