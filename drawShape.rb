db8b0d5d3aa1511347f9516775d0a86934cdab71
Thewisking <wissamdiab122@gmail.com>
2025-09-10 14:03:30 +0930

    drawshape and hello world

---
+++ drawShape.rb
@@ -0,0 +1,50 @@
+system("cls")
+
+print "Negawatt";puts  "Negawatt"
+print "Negawtt" # so print has an std::endl. im assuming not just \n
+
+
+puts "";puts "";puts "";puts "";
+
+
+
+puts "                   "; puts "                                 /\\                "
+puts "                   "; puts "                                /  \\               "
+puts "               /|  "; puts "                               /    \\              "
+puts "              / |  "; puts "                              /      \\             "
+puts "             /  |  "; puts "                             /        \\            "
+puts "            /   |  "; puts "                            /          \\           "
+puts "           /    |  "; puts "                           /            \\          "
+puts "          /     |  "; puts "                          /              \\         "
+puts "         /      |  "; puts "                         /                \\        "
+puts "        /       |  "; puts "                        /                  \\       "
+puts "       /        |  "; puts "                       /                    \\      "
+puts "      /         |  "; puts "                      /                      \\     "
+puts "     /          |  "; puts "                     /                        \\    "
+puts "    /           |  "; puts "                    /                          \\   "
+puts "   /            |  "; puts "                   /                            \\  "
+puts "  /             |  "; puts "                  /                              \\ "
+puts " /______________|  "; puts "                 /________________________________\\"
+
+
+
+puts "                                  /\\                  "; 
+puts "                                 / |\\                 "; 
+puts "                                /  | \\                "; 
+puts "               /|              /   |  \\               "; 
+puts "              / |             /    |   \\              "; 
+puts "             /  |            /     |    \\             "; 
+puts "            /   |           /      |     \\            "; 
+puts "           /    |          /       |      \\           "; 
+puts "          /     |         /        |       \\          "; 
+puts "         /      |        /         |        \\         "; 
+puts "        /       |       /          |         \\        "; 
+puts "       /        |      /           |          \\       "; 
+puts "      /         |     /            |           \\      "; 
+puts "     /          |    /             |            \\     "; 
+puts "    /           |   /              |             \\    "; 
+puts "   /            |  /               |              \\   "; 
+puts "  /             | /                |               \\  "; 
+puts " /______________|/_________________|________________\\ ";
+
+
